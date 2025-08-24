3074c256013b100a8382702d9372611aa4706d86
Chase <ct.chase.wang@gmail.com>
2025-08-24 15:53:39 -0400

    fix: updated nvim config

--- modules/config/nvim/lua/configs/lspconfig.lua
+++ modules/config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", 'pyright', 'ruff', 'hadolint'}
+local servers = { "html", "cssls", 'pyright', 'ruff', 'hadolint', 'gdscript'}
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for hanging options of lsp servers 
